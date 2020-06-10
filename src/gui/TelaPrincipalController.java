d9a1b5d8fef515ba63226d3aa4aee0982332d407
Alessandro Vaiz <alessandrovaiz@gmail.com>
2020-06-10 20:59:02 -0300

    Controller da tela principal

---
+++ src/gui/TelaPrincipalController.java
@@ -0,0 +1,97 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class TelaPrincipalController implements Initializable {
+
+	@FXML
+	private MenuItem menuIAcompanharEntregas;
+	@FXML
+	private MenuItem menuIVendedoresDisponiveis;
+	@FXML
+	private MenuItem menuIClientesVip;
+	@FXML
+	private MenuItem menuIEstoque;
+	@FXML
+	private MenuItem menuIProducao;
+	
+	
+	@FXML
+	private MenuItem menuINovoPedido;
+	@FXML
+	private MenuItem menuINovoCliente;
+	@FXML
+	private MenuItem menuINovoProduto;
+	@FXML
+	private MenuItem menuINovoEntregador;
+	
+	
+	
+	
+	@FXML
+	public void onMenuIAcompanharEntregasAction() {
+		System.out.println("teste");
+	}
+	
+	@FXML
+	public void onMenuIVendedoresDisponiveisAction() {
+		System.out.println("teste");
+	}
+	
+	@FXML
+	public void onMenuIClientesVipAction() {
+		System.out.println("teste");
+	}
+	
+	@FXML
+	public void onMenuIEstoqueAction() {
+		System.out.println("teste");
+	}
+	
+	@FXML
+	public void onMenuIProducaoAction() {
+		System.out.println("teste");
+	}
+	
+	
+	@FXML
+	public void onMenuINovoPedidoAction() {
+		System.out.println("teste");
+	}
+	
+	@FXML
+	public void onMenuINovoClienteAction() {
+		System.out.println("teste");
+	}
+	
+	@FXML
+	public void onMenuINovoProdutoAction() {
+		System.out.println("teste");
+	}
+	
+	
+	@FXML
+	public void onMenuINovoEntregadorAction() {
+		System.out.println("teste");
+	}
+	
+	
+	
+
+	
+	
+	
+	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
